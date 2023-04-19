b5fc53ad9ba3123e62d6d8d6b6d92d072aec7295
SuperSimpleDev <simon@supersimple.dev>
2023-04-19 08:55:58 -0400

    Add lesson 3 code

--- 2-copy-of-code/lesson-03/strings.js
+++ 2-copy-of-code/lesson-03/strings.js
@@ -1,2 +1,27 @@
 'hello'
 alert('hello');
+
+'some' + 'text'
+'some' + 'more' + 'text'
+
+typeof 2
+typeof 'hello'
+
+'hello' + 3
+'$' + 20.95 + 7.99
+'$' + (20.95 + 7.99)
+'$' + (2095 + 799) / 100
+
+'Items (' + (1 + 1) + '): $' + (2095 + 799) / 100
+alert('Items (' + (1 + 1) + '): $' + (2095 + 799) / 100);
+
+"hello"
+"I'm learning JavaScript"
+
+'I\'m learning JavaScript'
+alert('some\ntext');
+
+`hello`
+`Items (${1 + 1}): $${(2095 + 799) / 100}`
+`some
+text`
