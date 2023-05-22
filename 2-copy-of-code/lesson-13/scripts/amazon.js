af22774842e82e53532b4cc807e335fb5191d6a9
SuperSimpleDev <simon@supersimple.dev>
2023-05-21 23:39:04 -0400

    13d Solution

--- 2-copy-of-code/lesson-13/scripts/amazon.js
+++ 2-copy-of-code/lesson-13/scripts/amazon.js
@@ -72,6 +72,7 @@ document.querySelectorAll('.js-add-to-cart')
       const quantitySelector = document.querySelector(
         `.js-quantity-selector-${productId}`
       );
+      const quantity = quantitySelector.value;
 
       if (matchingItem) {
         matchingItem.quantity += 1;
