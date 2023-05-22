d8606b1a6fbcddb4638844331bb35ce3f2bc90cc
SuperSimpleDev <simon@supersimple.dev>
2023-05-21 23:49:12 -0400

    13g Solution

--- 2-copy-of-code/lesson-13/data/products.js
+++ 2-copy-of-code/lesson-13/data/products.js
@@ -656,5 +656,25 @@ const products = [
       "apparel",
       "mens"
     ]
+  },
+  {
+    id: "id1",
+    image: "images/products/backpack.jpg",
+    name: "Black Backpack",
+    rating: {
+      stars: 4.5,
+      count: 123
+    },
+    priceCents: 2500
+  },
+  {
+    id: "id2",
+    image: "images/products/umbrella.jpg",
+    name: "Large Green Umbrella",
+    rating: {
+      stars: 5,
+      count: 456
+    },
+    priceCents: 2999
   }
 ];
