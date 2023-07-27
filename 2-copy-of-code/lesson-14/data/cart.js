194094b9e325ab389227749470961f26bf34fb14
SuperSimpleDev <simon@supersimple.dev>
2023-07-27 03:10:25 -0400

    14l Solution

--- 2-copy-of-code/lesson-14/data/cart.js
+++ 2-copy-of-code/lesson-14/data/cart.js
@@ -58,3 +58,17 @@ export function calculateCartQuantity() {
 
   return cartQuantity;
 }
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
