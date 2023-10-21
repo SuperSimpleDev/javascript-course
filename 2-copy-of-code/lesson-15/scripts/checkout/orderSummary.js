74b116b945ca41c9a7563d321f131633fb9d9290
SuperSimpleDev <simon@supersimple.dev>
2023-10-20 20:25:33 -0400

    15h Solution

--- 2-copy-of-code/lesson-15/scripts/checkout/orderSummary.js
+++ 2-copy-of-code/lesson-15/scripts/checkout/orderSummary.js
@@ -120,11 +120,7 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-        container.remove();
-
+        renderOrderSummary();
         renderPaymentSummary();
       });
     });
