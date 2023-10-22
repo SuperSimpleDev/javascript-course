5abc90b0446d65dafd06d5351efeee6a560905b6
SuperSimpleDev <simon@supersimple.dev>
2023-10-21 22:31:46 -0400

    15m Solution

--- 2-copy-of-code/lesson-15/data/deliveryOptions.js
+++ 2-copy-of-code/lesson-15/data/deliveryOptions.js
@@ -26,12 +26,25 @@ export function getDeliveryOption(deliveryOptionId) {
   return deliveryOption;
 }
 
+function isWeekend(date) {
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
+
 export function calculateDeliveryDate(deliveryOption) {
-  const today = dayjs();
-  const deliveryDate = today.add(
-    deliveryOption.deliveryDays,
-    'days'
-  );
+  let remainingDays = deliveryOption.deliveryDays;
+  let deliveryDate = dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+
+    if (!isWeekend(deliveryDate)) {
+      remainingDays--;
+      // This is a shortcut for:
+      // remainingDays = remainingDays - 1;
+    }
+  }
+
   const dateString = deliveryDate.format(
     'dddd, MMMM D'
   );
