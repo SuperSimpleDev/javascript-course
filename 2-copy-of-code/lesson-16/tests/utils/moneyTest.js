27a205c88544c314bf22dc5efcf41120ee6d85be
SuperSimpleDev <simon@supersimple.dev>
2024-01-03 01:38:49 -0500

    16a Solution

--- 2-copy-of-code/lesson-16/tests/utils/moneyTest.js
+++ 2-copy-of-code/lesson-16/tests/utils/moneyTest.js
@@ -12,4 +12,8 @@ describe('test suite: formatCurrency', () => {
   it('rounds up to the nearest cent', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
   });
+
+  it('rounds down to the nearest cent', function() {
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
 });
