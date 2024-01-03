c036319724333025d0ff36df950685dc3df8240a
SuperSimpleDev <simon@supersimple.dev>
2024-01-03 14:32:44 -0500

    16j Solution

--- 2-copy-of-code/lesson-16/scripts/checkout/orderSummary.js
+++ 2-copy-of-code/lesson-16/scripts/checkout/orderSummary.js
@@ -93,12 +93,14 @@ export function renderOrderSummary() {
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
       html += `
-        <div class="delivery-option js-delivery-option"
+        <div class="delivery-option js-delivery-option
+          js-delivery-option-${matchingProduct.id}-${deliveryOption.id}"
           data-product-id="${matchingProduct.id}"
           data-delivery-option-id="${deliveryOption.id}">
           <input type="radio"
             ${isChecked ? 'checked' : ''}
-            class="delivery-option-input"
+            class="delivery-option-input
+            js-delivery-option-input-${matchingProduct.id}-${deliveryOption.id}"
             name="delivery-option-${matchingProduct.id}">
           <div>
             <div class="delivery-option-date">
