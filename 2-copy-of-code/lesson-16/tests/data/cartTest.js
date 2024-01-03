2016694488904831605b543e1edb7824e3e27e27
SuperSimpleDev <simon@supersimple.dev>
2024-01-03 15:11:01 -0500

    16k Solution

--- 2-copy-of-code/lesson-16/tests/data/cartTest.js
+++ 2-copy-of-code/lesson-16/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage, removeFromCart} from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
   beforeEach(() => {
@@ -86,6 +86,36 @@ describe('test suite: removeFromCart', () => {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 1,
       deliveryOptionId: '1'
+    }]));
+  });
+});
+
+describe('test suite: updateDeliveryOption', function() {
+  beforeEach(function() {
+    spyOn(localStorage, 'setItem');
+  });
+
+  it('updates the delivery option', function() {
+    spyOn(localStorage, 'getItem').and.callFake(function() {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    updateDeliveryOption('e43638ce-6aa0-4b85-b27f-e1d07eb678c6', '3');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '3'
     }]));
   });
 });
