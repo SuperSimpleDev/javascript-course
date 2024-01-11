738c43006e7649caa85da4f1c5c4f7c71495abaa
SuperSimpleDev <simon@supersimple.dev>
2024-01-11 03:08:28 -0500

    Switch test code to arrow functions

--- 2-copy-of-code/lesson-16/tests/checkout/orderSummaryTest.js
+++ 2-copy-of-code/lesson-16/tests/checkout/orderSummaryTest.js
@@ -1,11 +1,11 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
 
-describe('test suite: renderOrderSummary', function() {
+describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-  beforeEach(function() {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
 
     document.querySelector('.js-test-container').innerHTML = `
@@ -13,7 +13,7 @@ describe('test suite: renderOrderSummary', function() {
       <div class="js-payment-summary"></div>
     `;
 
-    spyOn(localStorage, 'getItem').and.callFake(function() {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: productId1,
         quantity: 2,
@@ -29,7 +29,7 @@ describe('test suite: renderOrderSummary', function() {
     renderOrderSummary();
   });
 
-  it('displays the cart', function() {
+  it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
     ).toEqual(2);
@@ -43,7 +43,7 @@ describe('test suite: renderOrderSummary', function() {
     document.querySelector('.js-test-container').innerHTML = '';
   });
 
-  it('removes a product', function() {
+  it('removes a product', () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
     expect(
       document.querySelectorAll('.js-cart-item-container').length
--- 2-copy-of-code/lesson-16/tests/data/cartTest.js
+++ 2-copy-of-code/lesson-16/tests/data/cartTest.js
@@ -1,10 +1,10 @@
 import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
-describe('test suite: addToCart', function() {
-  it('adds an existing product to the cart', function() {
+describe('test suite: addToCart', () => {
+  it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'setItem');
 
-    spyOn(localStorage, 'getItem').and.callFake(function() {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
         quantity: 1,
@@ -20,10 +20,10 @@ describe('test suite: addToCart', function() {
     expect(cart[0].quantity).toEqual(2);
   });
 
-  it('adds a new product to the cart', function() {
+  it('adds a new product to the cart', () => {
     spyOn(localStorage, 'setItem');
 
-    spyOn(localStorage, 'getItem').and.callFake(function() {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
     loadFromStorage();
