c3cc92e8a92e1698e24ca73cb56fbe148b89ae47
SuperSimpleDev <simon@supersimple.dev>
2024-04-30 14:17:01 -0400

    18h Solution

--- 2-copy-of-code/lesson-18/scripts/checkout.js
+++ 2-copy-of-code/lesson-18/scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {loadProducts, loadProductsFetch} from '../data/products.js';
-import {loadCart} from '../data/cart.js';
+import {loadCart, loadCartFetch} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
@@ -10,14 +10,7 @@ async function loadPage() {
     // throw 'error1';
 
     await loadProductsFetch();
-
-    const value = await new Promise((resolve, reject) => {
-      // throw 'error2';
-      loadCart(() => {
-        // reject('error3');
-        resolve('value3');
-      });
-    });
+    await loadCartFetch();
 
   } catch (error) {
     console.log('Unexpected error. Please try again later.');
