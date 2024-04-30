e17a3966e624cd53180bcd76830d39952c0a6b8f
SuperSimpleDev <simon@supersimple.dev>
2024-04-30 14:17:01 -0400

    18h Solution

--- 2-copy-of-code/lesson-18/data/cart.js
+++ 2-copy-of-code/lesson-18/data/cart.js
@@ -83,3 +83,10 @@ export function loadCart(fun) {
   xhr.open('GET', 'https://supersimplebackend.dev/cart');
   xhr.send();
 }
+
+export async function loadCartFetch() {
+  const response = await fetch('https://supersimplebackend.dev/cart');
+  const text = await response.text();
+  console.log(text);
+  return text;
+}
