6f04951559b5565b065c143ca2361388647656b8
SuperSimpleDev <simon@supersimple.dev>
2024-04-30 19:33:34 -0400

    18j Solution

--- 2-copy-of-code/lesson-18/tests/checkout/orderSummaryTest.js
+++ 2-copy-of-code/lesson-18/tests/checkout/orderSummaryTest.js
@@ -6,10 +6,8 @@ describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-  beforeAll((done) => {
-    loadProductsFetch().then(() => {
-      done();
-    });
+  beforeAll(async () => {
+    await loadProductsFetch();
   });
 
   beforeEach(() => {
