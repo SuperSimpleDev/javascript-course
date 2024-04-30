b237ab2d1ef3729b51550a280f2e5a6de8715ca0
SuperSimpleDev <simon@supersimple.dev>
2024-04-30 18:10:19 -0400

    Remove extra testing code

--- 2-copy-of-code/lesson-18/tests/checkout/orderSummaryTest.js
+++ 2-copy-of-code/lesson-18/tests/checkout/orderSummaryTest.js
@@ -1,25 +1,15 @@
 import {renderOrderSummary} from '../../scripts/checkout/orderSummary.js';
 import {loadFromStorage, cart} from '../../data/cart.js';
 import {loadProducts, loadProductsFetch} from '../../data/products.js';
-import {fakeProducts} from '../data/fakeProducts.js';
 
 describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
-  beforeAll(async () => {
-    spyOn(window, 'fetch').and.callFake(() => {
-      return new Promise((resolve) => {
-        const fakeResponse = {
-          json() {
-            return fakeProducts;
-          }
-        };
-        resolve(fakeResponse);
-      });
+  beforeAll((done) => {
+    loadProductsFetch().then(() => {
+      done();
     });
-
-    await loadProductsFetch();
   });
 
   beforeEach(() => {
