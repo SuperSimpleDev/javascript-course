f0b01ce443992f5d34234babf8e3053cee05527f
SuperSimpleDev <simon@supersimple.dev>
2024-05-07 02:23:09 -0400

    16m Solution

--- 2-copy-of-code/lesson-16/data/cart.js
+++ 2-copy-of-code/lesson-16/data/cart.js
@@ -1,3 +1,5 @@
+import {validDeliveryOption} from './deliveryOptions.js';
+
 export let cart;
 
 loadFromStorage();
@@ -68,6 +70,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   });
 
   if (!matchingItem) {
+    return;
+  }
+
+  if (!validDeliveryOption(deliveryOptionId)) {
     return;
   }
 
