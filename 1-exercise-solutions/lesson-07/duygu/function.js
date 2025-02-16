edc90062838c1c873251ff5c5af09a8335880a65
duyguozpercin <duygucetind@gmail.com>
2025-02-16 18:44:26 +0300

    done exercise 7c

--- 1-exercise-solutions/lesson-07/duygu/function.js
+++ 1-exercise-solutions/lesson-07/duygu/function.js
@@ -15,3 +15,18 @@ function greet(name) {
 greet("Simon");
 greet("John");
 greet("Angela");
+
+/* 7c */
+
+function greet(name) {
+  if (!name) {
+    console.log("Hi there!");
+  } else {
+    console.log(`Hello ${name}`);
+  }
+ 
+}
+
+greet("Duygu");
+greet("");
+greet();
