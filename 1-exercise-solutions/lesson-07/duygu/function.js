cf735083f10ce5bf33f01a477340c78fa83bb4ca
duyguozpercin <duygucetind@gmail.com>
2025-02-16 18:22:47 +0300

    first commit

---
+++ 1-exercise-solutions/lesson-07/duygu/function.js
@@ -0,0 +1,17 @@
+/* 7a */
+
+function greet() {
+  console.log("Hello!");
+}
+
+greet();
+
+/* 7b */
+
+function greet(name) {
+  console.log(`Hello ${name}!`);
+}
+
+greet("Simon");
+greet("John");
+greet("Angela");
