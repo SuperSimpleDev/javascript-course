b3c280d63b58b694982c479e40abaea4cfe13252
duyguozpercin <duygucetind@gmail.com>
2025-02-16 19:08:32 +0300

    done exercise 7e

--- 1-exercise-solutions/lesson-07/duygu/function.js
+++ 1-exercise-solutions/lesson-07/duygu/function.js
@@ -30,3 +30,21 @@ function greet(name) {
 greet("Duygu");
 greet("");
 greet();
+
+/* 7d */
+
+function convertToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9/5) + 32;
+  console.log(`${celsius} degree is ${fahrenheit} fahrenheit.`);
+}
+
+convertToFahrenheit(25);
+
+/* 7e */
+
+function convertToCelsius (fahrenheit) {
+  let celsius = (fahrenheit -32) * 5 / 9;
+  console.log(`${fahrenheit} is ${celsius} celsius.`);
+}
+
+convertToCelsius (86);
